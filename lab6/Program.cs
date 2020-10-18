a27034465cdcfb70957785503dd6217858534f67
Victor Shulpov <vshulpov@list.ru>
2020-10-18 15:28:41 +0700

    car and engine classes

--- lab6/Program.cs
+++ lab6/Program.cs
@@ -64,4 +64,206 @@ namespace lab6
 
 	}
 	/////////////////////////////////////////////////
+	class Car
+	{
+		private String name;
+		private int price;
+		private String color;
+		private int speed;
+		private int benzine;
+		private Engine engine;
+		public void init(String name, int price, String color, int speed, int benzine, Engine engine)
+		{
+			this.name = name;
+			this.price = price;
+			this.color = color;
+			this.speed = speed;
+			this.benzine = benzine;
+			this.engine = engine;
+			Console.WriteLine("Car initialized!");
+		}
+		public void readCarData()
+		{
+			string str = "";
+			int number = 0;
+			
+			Console.WriteLine("ENTER CAR DATA:");
+			//////////////////////////////////////////////////
+			Console.WriteLine("\tname:\t");
+			this.name = Console.ReadLine();
+			///////////////////////////////////////////////
+			bool flag = false; //флаг правильности ввода чисел
+			while (!flag) {
+				Console.WriteLine("\tprice:\t");
+				str = Console.ReadLine();
+				flag = int.TryParse(str, out number);
+				if (!flag)
+				{
+					Console.WriteLine("re-enter:");
+				}
+
+			}
+			this.price = number;
+			//////////////////////////////////////////////
+
+			Console.WriteLine("\tcolor:\t");
+			this.color = Console.ReadLine();
+			//////////////////////////////////////////////////
+			flag = false;
+			while (!flag)
+			{
+				Console.WriteLine("\tengineRPM:\t");
+				str = Console.ReadLine();
+				flag = int.TryParse(str, out number);
+				if (!flag)
+				{
+					Console.WriteLine("re-enter:");
+				}
+
+			}
+			this.engine.setEngineRPM(number);
+			////////////////////////////////////////////////////
+			flag = false;
+			while (!flag)
+			{
+				Console.WriteLine("\tcapacity:\t");
+				str = Console.ReadLine();
+				flag = int.TryParse(str, out number);
+				if (!flag)
+				{
+					Console.WriteLine("re-enter:");
+				}
+
+			}
+			this.engine.setCapacity(number);
+			///////////////////////////////////////////////////////
+			flag = false;
+			while (!flag)
+			{
+				Console.WriteLine("\tengine power:\t");
+				str = Console.ReadLine();
+				flag = int.TryParse(str, out number);
+				if (!flag)
+				{
+					Console.WriteLine("re-enter:");
+				}
+
+			}
+			this.engine.setEnginePower(number);
+			///////////////////////////////////////////////////////
+			flag = false;
+			while (!flag)
+			{
+				Console.WriteLine("\tquantity of cylinders:\t");
+				str = Console.ReadLine();
+				flag = int.TryParse(str, out number);
+				if (!flag)
+				{
+					Console.WriteLine("re-enter:");
+				}
+
+			}
+			this.engine.setQuantityOfCylinders(number);
+			///////////////////////////////////////////////////////
+			flag = false;
+			while (!flag)
+			{
+				Console.WriteLine("\tspeed:\t");
+				str = Console.ReadLine();
+				flag = int.TryParse(str, out number);
+				if (!flag)
+				{
+					Console.WriteLine("re-enter:");
+				}
+
+			}
+			this.speed = number;
+			///////////////////////////////////////////////////////
+			flag = false;
+			while (!flag)
+			{
+				Console.WriteLine("\tbenzine:\t");
+				str = Console.ReadLine();
+				flag = int.TryParse(str, out number);
+				if (!flag)
+				{
+					Console.WriteLine("re-enter:");
+				}
+
+			}
+			this.benzine = number;
+			///////////////////////////////////////////////////////
+
+
+
+		}
+		public void displayDataCar()
+		{
+				Console.WriteLine("\n\tCAR DATA");
+				Console.WriteLine("\t\tName:\t" + this.name);
+				Console.WriteLine("\t\tPrice:\t" + this.price);
+				Console.WriteLine("\t\tColor:\t" + this.color);
+				Console.WriteLine("\t\tEngineRPM:\t" + this.engine.getEngineRPM());
+				Console.WriteLine("\t\tCapacity:\t" + this.engine.getCapacity());
+				Console.WriteLine("\t\tEngine Power:\t" + this.engine.getEnginePower());
+				Console.WriteLine("\t\tQuanity of cylinders:\t" + this.engine.getQuantityOfCylinders());
+				Console.WriteLine("\t\tBenzine:\t" + this.benzine);
+				Console.WriteLine("\t\tSpeed:\t" + this.speed);
+
+		}
+		public void addBenzine(int liters)
+		{
+			Console.WriteLine(liters + "lit. benzine added!");
+			this.benzine += liters;
+		}
+		public void startEngine()
+		{
+			if (this.benzine > 0)
+			{
+				this.engine.setEngineRPM(800);
+				Console.WriteLine("Engine started!");
+			}
+			else
+			{
+				Console.WriteLine("No benzine. Engine didn't start!");
+			}
+		}
+		public void stopEngine()
+		{
+			if (this.engine.getEngineRPM() > 0)
+			{
+				this.engine.setEngineRPM(0);
+				Console.WriteLine("Engine stopped!");
+			}
+			else
+			{
+				Console.WriteLine("Engine stopped already!");
+			}
+		}
+		public void addSpeed(int speed)
+		{
+			if (this.engine.getEngineRPM() > 0)
+			{
+				this.speed += speed;
+				Console.WriteLine("Car speeded up!");
+			}
+			else
+			{
+				Console.WriteLine("Engine isn't starting. Car didn't speed up!");
+			}
+		}
+		public void reduceSpeed(int speed)
+		{
+			if (this.speed > 0)
+			{
+				this.speed -= speed;
+				Console.WriteLine("Car speeded down!");
+			}
+			else
+			{
+				Console.WriteLine("Car is parking. Car didn't speed down!");
+			}
+		}
+	}
+	/////////////////////////////////////////////////
 }
