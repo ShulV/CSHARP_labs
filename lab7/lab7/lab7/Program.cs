f9d114d459596f08461a3e961c9e7f9c25f56bdd
Victor Shulpov <vshulpov@list.ru>
2020-11-03 19:51:31 +0700

    string, properties, objects array

--- lab7/lab7/lab7/Program.cs
+++ lab7/lab7/lab7/Program.cs
@@ -1,75 +1,38 @@
-﻿/*
- 1) Для придуманной вами задачи (Пункт 1 из задания к л/р 2 + пункт 1 из задания к л/р 4) реализовать необходимые классы на языке C#;
-2) Продемонстрировать их использование в консольном С# приложении;
-3) Продемонстрировать сборку проекта и запуск приложения на платформе Mono под Linux, Mac OS и пр.;
-!) Использование GIT обязательно.
-Поля класса «машина»:
-•	Название
-•	Цена
-•	Цвет
-•	Скорость
-•	Количество бензина
-•	Двигатель (объект)
-Функции:
-•	Инициализация
-•	Установка параметров автомобиля
-•	Вывод данных машины
-•	Запуск двигателя
-•	Остановка двигателя
-•	Добавление скорости
-•	Уменьшить скорость
-Поля класса «двигатель»:
-•	Количество оборотов в минуту
-•	Мощность в Л.С.
-•	Объем в см куб.
-•	Количество цилиндров
-Функции:
-•	Инициализация
-•	Сеттеры и геттеры
-*/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab6
+namespace lab7
 {
 	/////////////////////////////////////////////////
 	class Program
-    {
-        static void Main(string[] args)
-        {
+	{
+		static void Main(string[] args)
+		{
 			//Console.WriteLine("Kek lol program is working");
-			Car bmw_x6 = new Car();
-			Engine bmw_engine = new Engine();
-			bmw_engine.init(0, 4395, 625, 8);
-			bmw_x6.init("BMW X6", 3500000, "BLACK", 0, 0, bmw_engine); //инициализируем поля объекта
-			bmw_x6.displayDataCar();
-			bmw_x6.readCarData();
-			bmw_x6.displayDataCar();
-			bmw_x6.startEngine(); //пытаемся завести двигатель
-			bmw_x6.displayDataCar();
-			bmw_x6.addBenzine(10); //добавляем бензин
-			bmw_x6.displayDataCar();
-			bmw_x6.startEngine(); //снова пытаемся завести двигатель
-			bmw_x6.displayDataCar();
-			for (int i = 0; i < 4; i++)
-			{
-				bmw_x6.addSpeed(i * 5); //добавляем скорость
-				bmw_x6.displayDataCar();
-			}
-			for (int i = 0; i < 4; i++)
-			{
-				bmw_x6.reduceSpeed(i * 5); //убавляем скорость
-				bmw_x6.displayDataCar();
-			}
 
-			bmw_x6.stopEngine(); //останавливаем двигатель
-			bmw_x6.displayDataCar();
+			Car bmw = new Car();
+			Car audi = new Car();
+			Engine engine1 = new Engine();
+			Engine engine2 = new Engine();
+			engine1.init(0, 4395, 625, 8);
+			engine2.init(0, 4395, 625, 8);
+			Car[] car = new Car[2];
+			bmw.init("BMW X6", 3500000, "BLACK", 0, 0, engine1); //инициализируем поля объекта
+			audi.init("Audi", 3300000, "BLACK", 0, 0, engine2); //инициализируем поля объекта
+			audi.readCarData();
+			car[0] = bmw;
+			car[1] = audi;
+			car[0].displayDataCar();
+			car[1].displayDataCar();
+	
+			Console.WriteLine("\tВторой цвет: " + car[0].second_color);
+		
 			Console.ReadKey();
 		}
-    }
+	}
 	/////////////////////////////////////////////////
 	class Engine
 	{
@@ -128,11 +91,14 @@ namespace lab6
 		private int speed;
 		private int benzine;
 		private Engine engine;
+
+		public String second_color { private set; get; }
 		public void init(String name, int price, String color, int speed, int benzine, Engine engine)
 		{
 			this.name = name;
 			this.price = price;
 			this.color = color;
+			this.second_color = color;
 			this.speed = speed;
 			this.benzine = benzine;
 			this.engine = engine;
@@ -142,14 +108,15 @@ namespace lab6
 		{
 			string str = "";
 			int number = 0;
-			
+
 			Console.WriteLine("ENTER CAR DATA:");
 			//////////////////////////////////////////////////
 			Console.Write("\tname:\t");
 			this.name = Console.ReadLine();
 			///////////////////////////////////////////////
 			bool flag = false; //флаг правильности ввода чисел
-			while (!flag) {
+			while (!flag)
+			{
 				Console.Write("\tprice:\t");
 				str = Console.ReadLine();
 				flag = int.TryParse(str, out number);
@@ -255,16 +222,16 @@ namespace lab6
 		}
 		public void displayDataCar()
 		{
-				Console.WriteLine("\n\tCAR DATA");
-				Console.WriteLine("\t\tName:\t" + this.name);
-				Console.WriteLine("\t\tPrice:\t" + this.price);
-				Console.WriteLine("\t\tColor:\t" + this.color);
-				Console.WriteLine("\t\tEngineRPM:\t" + this.engine.getEngineRPM());
-				Console.WriteLine("\t\tCapacity:\t" + this.engine.getCapacity());
-				Console.WriteLine("\t\tEngine Power:\t" + this.engine.getEnginePower());
-				Console.WriteLine("\t\tQuanity of cylinders:\t" + this.engine.getQuantityOfCylinders());
-				Console.WriteLine("\t\tBenzine:\t" + this.benzine);
-				Console.WriteLine("\t\tSpeed:\t" + this.speed);
+			Console.WriteLine("\n\tCAR DATA");
+			Console.WriteLine("\t\tName:\t" + this.name.ToLower());
+			Console.WriteLine("\t\tPrice:\t" + this.price);
+			Console.WriteLine("\t\tColor:\t" + this.color.ToLower());
+			Console.WriteLine("\t\tEngineRPM:\t" + this.engine.getEngineRPM());
+			Console.WriteLine("\t\tCapacity:\t" + this.engine.getCapacity());
+			Console.WriteLine("\t\tEngine Power:\t" + this.engine.getEnginePower());
+			Console.WriteLine("\t\tQuanity of cylinders:\t" + this.engine.getQuantityOfCylinders());
+			Console.WriteLine("\t\tBenzine:\t" + this.benzine);
+			Console.WriteLine("\t\tSpeed:\t" + this.speed);
 
 		}
 		public void addBenzine(int liters)
