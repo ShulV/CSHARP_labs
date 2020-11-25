d04a41c878169858a64f175c2f69d91f89b45c01
Victor Shulpov <vshulpov@list.ru>
2020-11-25 10:04:37 +0700

    init заменил на конструкторы с разным кол-вом параметром, продемонстрировал, создания массива объектов через конструктор с одним параметром

--- lab7/lab7/lab7/Program.cs
+++ lab7/lab7/lab7/Program.cs
@@ -6,39 +6,126 @@ using System.Threading.Tasks;
 
 namespace lab7
 {
-	/////////////////////////////////////////////////
+	// lab work №7,8,9 Shulpov Victor PI-92
+	/*
+	Реализовать работу автомобиля на примере динамической структуры.
+	Поля структуры:
+	•	Название (строка)
+	•	Цена (целое)
+	•	Цвет (строка)
+	•	Количество оборотов двигателя в минуту (целое)
+	•	Скорость (целое)
+	•	Количество бензина (целое)
+	Функции:
+	•	Инициализация
+	•	Установка параметров автомобиля
+	•	Вывод данных машины
+	•	Запуск двигателя
+	•	Остановка двигателя
+	•	Добавление скорости
+	•	Уменьшить скорость
+	*/
+
+
 	class Program
 	{
+
 		static void Main(string[] args)
 		{
 			//Console.WriteLine("Kek lol program is working");
+			String str_in;
+			int choice = 1;
+			bool flag_in = false;
+			while (choice != 0)
+			{
+				flag_in = false;
+				Console.WriteLine("\n\nВведите 1 - ПОКАЗАТЬ 7 ЛАБУ\nВведите 2 - ПОКАЗАТЬ 8 ЛАБУ\nВведите 3 - ПОКАЗАТЬ 9 ЛАБУ\nВведите 0 - ВЫХОД\nваш выбор: ");
+				while (!flag_in)
+				{
+					str_in = Console.ReadLine();
+					flag_in = int.TryParse(str_in, out choice);
+					if (!flag_in)
+					{
+						Console.Write("re-enter:");
+					}
 
-			Car bmw = new Car();
-			Car audi = new Car();
-			Engine engine1 = new Engine();
-			Engine engine2 = new Engine();
-			engine1.init(0, 4395, 625, 8);
-			engine2.init(0, 4395, 625, 8);
-			Car[] car = new Car[2];
-			bmw.init("BMW X6", 3500000, "BLACK", 0, 0, engine1); //инициализируем поля объекта
-			audi.init("Audi", 3300000, "BLACK", 0, 0, engine2); //инициализируем поля объекта
-			audi.readCarData();
-			car[0] = bmw;
-			car[1] = audi;
-			car[0].displayDataCar();
-			car[1].displayDataCar();
-			car[1] = car[1] + 50;
-			car[1].displayDataCar();
-			car[1]++;
-			car[1].displayDataCar();
-			Console.WriteLine("\tВторой цвет: " + car[0].secondColor);
-			String str = "";
-			car[1].getStr(out str);
-			Console.WriteLine("out str="+str);
-			car[1].changeStr(ref str);
-			Console.WriteLine("ref str=" + str);
+				}
+				if (choice == 0) { break; }
+				if (choice == 1)
+				{
+					Engine engine1 = new Engine(0, 4395, 625, 8);
+					Engine engine2 = new Engine(0, 4395, 625, 8);
+					Car bmw = new Car("BMW X6", 3500000, "BLACK", 0, 0, engine1); //инициализируем поля объекта
+					Car audi = new Car("Audi", 3300000, "BLACK", 0, 0, engine2); //инициализируем поля объекта);
+					
+					Car[] car = new Car[2];
+					
+					audi.readCarData();
+					car[0] = bmw;
+					car[1] = audi;
+					car[0].displayDataCar();
+					car[1].displayDataCar();
+					car[1] = car[1] + 50;
+					car[1].displayDataCar();
 
-			Console.ReadKey();
+					Car test_car = car[1];
+					Console.WriteLine("test_car.benzine " + test_car.benzine);
+					test_car = car[1]++;
+					Console.WriteLine("car[1]++ = " + test_car.benzine);
+					test_car = ++car[1];
+					Console.WriteLine("++car[1] = " + test_car.benzine);
+					
+					car[1].displayDataCar();
+					
+					Console.WriteLine("\tВторой цвет: " + car[0].secondColor);
+					String str = "";
+					car[1].getStr(out str);
+					Console.WriteLine("out str=" + str);
+					car[1].changeStr(ref str);
+					Console.WriteLine("ref str=" + str);
+					Console.ReadKey();
+				}
+				if (choice == 2)
+                {
+					
+					Console.WriteLine("count =" + Car.getCount());
+					Car bmw = new Car();
+					Console.WriteLine("После создания одного статического объекта Car\ncount =" + Car.getCount() + "\n");
+					Car[] bmws = new Car[5];
+					for(int i = 0; i < 5; i++)
+                    {
+						bmws[i] = new Car();
+                    }
+
+					Console.WriteLine("После создания массива из 5 статических объектов Car\ncount =" + Car.getCount() +  "\n");
+					Console.ReadKey();
+				}
+				if (choice == 3)
+                {
+					Console.WriteLine("конструктор 0 парам");
+					Car empty_car = new Car();//0 парам
+					empty_car.displayDataCar();
+					Console.WriteLine("конструктор 1 парам");
+					Car some_car = new Car("lada");//один парам
+					some_car.displayDataCar();
+					Console.WriteLine("конструктор все парам");
+					Engine engine1 = new Engine(0, 4395, 625, 8);
+					Car bmw = new Car("BMW X6", 3500000, "BLACK", 0, 0, engine1); //много парам
+					bmw.displayDataCar();
+					Console.WriteLine("\tМассив объектов");
+					Car[] car = new Car[3];
+					for(int i=0; i < 3; i++)
+                    {
+						car[i] = new Car("car" + i.ToString());
+						car[i].displayDataCar();
+                    }
+
+				}
+			}
+			
+			
+
+			
 		}
 	}
 	/////////////////////////////////////////////////
@@ -48,8 +135,24 @@ namespace lab7
 		private int capacity; //объем см куб
 		private int enginePower; //мощность Л.С.
 		private int quantityOfCylinders; //количество цилиндров
-
-		public void init(int engineRPM, int capacity, int enginePower, int quantityOfCylinders)
+		//конструктор без параметров
+		public Engine()
+        {
+			this.engineRPM = 0;
+			this.capacity = 0;
+			this.enginePower = 0;
+			this.quantityOfCylinders = 0;
+		}
+		//конструктор с одним параметром
+		public Engine(int engineRPM)
+		{
+			this.engineRPM = engineRPM;
+			this.capacity = 0;
+			this.enginePower = 0;
+			this.quantityOfCylinders = 0;
+		}
+		//конструктор со всеми параметрами
+		public Engine(int engineRPM, int capacity, int enginePower, int quantityOfCylinders)
 		{
 			this.engineRPM = engineRPM;
 			this.capacity = capacity;
@@ -97,8 +200,58 @@ namespace lab7
 		private int price;
 		private String color;
 		private int speed;
-		private int benzine;
+		public int benzine;
 		private Engine engine;
+		static int count=0;// определение статической переменной-члена класса
+
+
+		public Car()
+        {
+			this.name = "";
+			this.price = 0;
+			this.color = "";
+			this.speed = 0;
+			this.benzine = 0;
+			this.engine = null;
+			count++;
+			Console.WriteLine("Car created 0 param!");
+		}
+
+		//конструктор с одним параметром
+		public Car(String name)
+		{
+			this.name = name;
+			this.price = 0;
+			this.color = "";
+			this.secondColor = "";
+			this.speed = 0;
+			this.benzine = 0;
+			this.engine = null;
+			count++;
+			Console.WriteLine("Car created 1 param!");
+		}
+
+		//конструктор со всеми параметрами
+		public Car(String name, int price, String color, int speed, int benzine, Engine engine)
+		{
+			this.name = name;
+			this.price = price;
+			this.color = color;
+			this.secondColor = color;
+			this.speed = speed;
+			this.benzine = benzine;
+			this.engine = engine;
+			count++;
+			Console.WriteLine("Car created all param!");
+		}
+		public static int getCount()
+        {
+			return count;
+        }
+		~Car()
+		{
+			count--;
+		}
 
 		/*
 		 Ref параметры предназначены для данных, которые могут быть изменены, 
@@ -114,17 +267,7 @@ namespace lab7
 			str = str + "!!!";
         }
 		public String secondColor { private set; get; }
-		public void init(String name, int price, String color, int speed, int benzine, Engine engine)
-		{
-			this.name = name;
-			this.price = price;
-			this.color = color;
-			this.secondColor = color;
-			this.speed = speed;
-			this.benzine = benzine;
-			this.engine = engine;
-			Console.WriteLine("Car initialized!");
-		}
+		
 		public void readCarData()
 		{
 			string str = "";
@@ -247,10 +390,10 @@ namespace lab7
 			Console.WriteLine("\t\tName:\t" + this.name.ToLower());
 			Console.WriteLine("\t\tPrice:\t" + this.price);
 			Console.WriteLine("\t\tColor:\t" + this.color.ToLower());
-			Console.WriteLine("\t\tEngineRPM:\t" + this.engine.getEngineRPM());
-			Console.WriteLine("\t\tCapacity:\t" + this.engine.getCapacity());
-			Console.WriteLine("\t\tEngine Power:\t" + this.engine.getEnginePower());
-			Console.WriteLine("\t\tQuanity of cylinders:\t" + this.engine.getQuantityOfCylinders());
+			//Console.WriteLine("\t\tEngineRPM:\t" + this.engine.getEngineRPM());
+			//Console.WriteLine("\t\tCapacity:\t" + this.engine.getCapacity());
+			//Console.WriteLine("\t\tEngine Power:\t" + this.engine.getEnginePower());
+			//Console.WriteLine("\t\tQuanity of cylinders:\t" + this.engine.getQuantityOfCylinders());
 			Console.WriteLine("\t\tBenzine:\t" + this.benzine);
 			Console.WriteLine("\t\tSpeed:\t" + this.speed);
 
@@ -315,10 +458,10 @@ namespace lab7
 		}
 		public static Car operator ++(Car car) // ++ один префикс и постфикс!
 		{
-			++car.benzine;
-			return car;
+			return new Car{ benzine = car.benzine + 1};
 		}
 
 	}
+
 	/////////////////////////////////////////////////
 }
